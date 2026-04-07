1fd3204ffc1c08baad42d98821be8d6d440c3f10
kickymaulana <kickymaulana@gmail.com>
2026-04-07 14:12:39 +0700

    menambah fungsi logout

--- resources/js/components/NavUser.vue
+++ resources/js/components/NavUser.vue
@@ -28,6 +28,20 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import { Link } from '@inertiajs/vue3'
+
 interface User {
   name: string
   email: string
@@ -44,71 +58,107 @@ const { isMobile } = useSidebar()
 <template>
   <SidebarMenu>
     <SidebarMenuItem>
-      <DropdownMenu>
-        <DropdownMenuTrigger as-child>
-          <SidebarMenuButton
-            size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-          >
-            <Avatar class="h-8 w-8 rounded-lg grayscale">
-              <AvatarImage :src="user.avatar" :alt="user.name" />
-              <AvatarFallback class="rounded-lg">
-                CN
-              </AvatarFallback>
-            </Avatar>
-            <div class="grid flex-1 text-left text-sm leading-tight">
-              <span class="truncate font-medium">{{ user.name }}</span>
-              <span class="text-muted-foreground truncate text-xs">
-                {{ user.email }}
-              </span>
-            </div>
-            <IconDotsVertical class="ml-auto size-4" />
-          </SidebarMenuButton>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          class="w-(--reka-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-          :side="isMobile ? 'bottom' : 'right'"
-          :side-offset="4"
-          align="end"
-        >
-          <DropdownMenuLabel class="p-0 font-normal">
-            <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-              <Avatar class="h-8 w-8 rounded-lg">
+
+    <AlertDialog>
+
+
+
+        <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+            <SidebarMenuButton
+                size="lg"
+                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+                <Avatar class="h-8 w-8 rounded-lg grayscale">
                 <AvatarImage :src="user.avatar" :alt="user.name" />
                 <AvatarFallback class="rounded-lg">
-                  CN
+                    CN
                 </AvatarFallback>
-              </Avatar>
-              <div class="grid flex-1 text-left text-sm leading-tight">
+                </Avatar>
+                <div class="grid flex-1 text-left text-sm leading-tight">
                 <span class="truncate font-medium">{{ user.name }}</span>
                 <span class="text-muted-foreground truncate text-xs">
-                  {{ user.email }}
+                    {{ user.email }}
                 </span>
-              </div>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <IconUserCircle />
-              Account
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <IconCreditCard />
-              Billing
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <IconNotification />
-              Notifications
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <IconLogout />
-            Log out
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+                </div>
+                <IconDotsVertical class="ml-auto size-4" />
+            </SidebarMenuButton>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+                class="w-(--reka-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                :side="isMobile ? 'bottom' : 'right'"
+                :side-offset="4"
+                align="end"
+                >
+                <DropdownMenuLabel class="p-0 font-normal">
+                    <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar class="h-8 w-8 rounded-lg">
+                        <AvatarImage :src="user.avatar" :alt="user.name" />
+                        <AvatarFallback class="rounded-lg">
+                        CN
+                        </AvatarFallback>
+                    </Avatar>
+                    <div class="grid flex-1 text-left text-sm leading-tight">
+                        <span class="truncate font-medium">{{ user.name }}</span>
+                        <span class="text-muted-foreground truncate text-xs">
+                        {{ user.email }}
+                        </span>
+                    </div>
+                    </div>
+                </DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                    <IconUserCircle />
+                    Account
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                    <IconCreditCard />
+                    Billing
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                    <IconNotification />
+                    Notifications
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+
+                <DropdownMenuSeparator />
+
+                <AlertDialogTrigger as-child>
+                    <DropdownMenuItem @select.prevent>
+                        <IconLogout class="size-4" />
+                        Log out
+                    </DropdownMenuItem>
+                </AlertDialogTrigger>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Keluar?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    Apakah anda yakin ingin keluar dari aplikasi
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction as-child>
+                <Link
+                    :href="route('logout')"
+                    method="post"
+                    as="button"
+                >
+                    Ya, Keluar
+                </Link>
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+
+    </AlertDialog>
+
     </SidebarMenuItem>
   </SidebarMenu>
 </template>
