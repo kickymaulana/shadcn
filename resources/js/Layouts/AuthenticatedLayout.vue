5999f0304dd66d2aa5421e3f90adc9eb042af6d0
kickymaulana <kickymaulana@gmail.com>
2026-04-07 10:31:50 +0700

    menerapkan Persistent Layou

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppSidebar from "@/components/AppSidebar.vue"
+import SiteHeader from "@/components/SiteHeader.vue"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+</script>
+
+<template>
+  <SidebarProvider
+    :style="{
+      '--sidebar-width': 'calc(var(--spacing) * 72)',
+      '--header-height': 'calc(var(--spacing) * 12)',
+    }"
+  >
+    <AppSidebar variant="inset" />
+
+    <SidebarInset>
+      <SiteHeader />
+
+      <main class="flex flex-1 flex-col pt-6 @container/main">
+        <slot />
+      </main>
+
+    </SidebarInset>
+  </SidebarProvider>
+</template>
